1f1a48d69f26a137f41b41a69eb64fdff1b5b0c4
shashidharyadav252 <126072499+shashidharyadav252@users.noreply.github.com>
2024-07-19 10:43:45 +0530

    Add files via upload

---
+++ concate.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 11 13:30:02 2024
+
+@author: ML LAB
+"""
+
+import numpy as np
+tensor_2d=np.array([[1,2,3],[4,5,6]])
+tensor_2d=np.array([[7,8,9],[0],2]])
